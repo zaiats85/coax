41250248a7f116a4b51fe44f854e18495efdb0c4
zaiats85 <zaiats85@mail.ru>
2017-07-11 10:44:59 +0300

    initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ACCESS_TOKEN', "156391772-lPlhiKxHkVtwvs9sicUEdGsLHsWHjrpGEkXnJvji");
+define('ACCESS_TOKEN_SECRET', "L8NU7ir4c8DJuoaiavMXg3z4FHkP6AlGJHLVV84LCp1BN");
+define('CONSUMER_KEY', 'HncoUz6vCIIi1TFid6a8bzWGh');
+define('CONSUMER_SECRET', "nqxQsPLh8pu4PCGIOyqWkwNr9tslk7j4u1MHOwlack5SAOKl5O");
+
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+include './config.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
+
+$twig = new Twig_Environment($loader, array());
+
+if (isset($_POST['query']) && $_SERVER["REQUEST_METHOD"] == "POST"){
+    $query = filter_var($_POST['query'], FILTER_SANITIZE_STRING);
+
+//    $pest = new Pest('https://api.twitter.com/1.1/search/tweets.json');
+    
+
+
+    $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+
+    $tweets = $connection->get('search/tweets', array(
+        'q'=>$query
+    ));
+
+
+
+    echo $twig->render('form.twig', array(
+        'tweets'=> $tweets->statuses,
+        'errors'=>$tweets->errors,
+        'meta'=>$tweets->search_metadata
+    ));
+} else {
+    echo $twig->render('form.twig');
+}
+
+
